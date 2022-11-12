fe69331fc7e7e447ea33a403f6e982b059f44b2c
Kazeem Abolaji Kadiri <kazeem.kadiri@gmail.com>
2022-11-12 10:42:07 +0100

    fix: file exists returns an error

--- controllers/csvController.js
+++ controllers/csvController.js
@@ -1,30 +1,45 @@
 const path = require("path");
 const fs = require("fs");
+const escapeFilename = require('escape-filename');
 
 const handleCsv = async (req, res) => {
   const files = req.files;
   if (files) {
     const file = files.file;
     const fileName = files.file.name;
-    const directory = "../uploads";
-    if (!fs.existsSync([directory])) {
-      //starting point, directory is expected not to exist, so make directory.
-      fs.rmSync("uploads", { recursive: true }, (err) => {
-        //delete directory to make sure it does not exist. To avoid having more than 1 file in directory
-        if (err) throw err;
-      });
-      fs.mkdirSync("uploads"); //creates or recreates directory depending on the state of directory
-    }
-    const filePath = path.join(__dirname, directory, fileName);
+    const uploadsDirectory = path.join(__dirname, '..', 'uploads');
 
-    file.mv(filePath, (err) => {
-      //save file in uploads directory
-      if (err) {
-        res.status(500).json({ error: err });
-      } else {
-        res.status(200).json({ message: "file uploaded" });
+    const filePath = path.join( uploadsDirectory, escapeFilename.escape(fileName) );
+
+    try{
+      // if file exists, an error is sent back
+      if( fs.existsSync(filePath) ){
+
+        return res.status(400).json({ message: "File exists" }).end();
+
       }
-    });
+
+      // if uploads directory does not exist, create it 
+      if (!fs.existsSync( uploadsDirectory )) {
+
+        //creates or recreates directory depending on the state of directory
+        fs.mkdirSync( uploadsDirectory ); 
+
+      }
+
+      file.mv(filePath, (err) => {
+        //save file in uploads directory
+        if (err) res.status(500).json({ error: err });
+        
+        res.status(200).json({ message: "file uploaded" });
+      });
+
+    }catch(err){
+
+      res.status(500).json({error: err});
+
+    }
+
   }
 };
 
