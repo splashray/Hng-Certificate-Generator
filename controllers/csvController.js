54be566f78bd7cd729717029cccc45a27f3f243c
Intuneteq <97626624+Intuneteq@users.noreply.github.com>
2022-11-12 07:32:13 +0100

    FIX: multiple files in upload directory fix

--- controllers/csvController.js
+++ controllers/csvController.js
@@ -7,12 +7,18 @@ const handleCsv = async (req, res) => {
     const file = files.file;
     const fileName = files.file.name;
     const directory = "../uploads";
-    if (!fs.existsSync([directory])) {  //starting point, directory is expected not to exist, so make directory.
-      fs.mkdirSync("uploads");
+    if (!fs.existsSync([directory])) {
+      //starting point, directory is expected not to exist, so make directory.
+      fs.rmSync("uploads", { recursive: true }, (err) => {
+        //delete directory to make sure it does not exist. To avoid having more than 1 file in directory
+        if (err) throw err;
+      });
+      fs.mkdirSync("uploads"); //creates or recreates directory depending on the state of directory
     }
     const filePath = path.join(__dirname, directory, fileName);
 
-    file.mv(filePath, (err) => {  //save file in uploads directory
+    file.mv(filePath, (err) => {
+      //save file in uploads directory
       if (err) {
         res.status(500).json({ error: err });
       } else {
